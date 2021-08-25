8403b1e5bf7111dc7bb2a61ee03aa7b4eeb8d37d
Taras Dovbyus <79213421+dstaras@users.noreply.github.com>
2021-08-25 17:29:45 +0300

    finish checked period

--- src/components/HistoryHeaderBtns/HistoryHeaderBtns.jsx
+++ src/components/HistoryHeaderBtns/HistoryHeaderBtns.jsx
@@ -1,15 +1,15 @@
 import LabelInput from "../_share/LabelInput/LabelInput";
 import s from "./HistoryHeaderBtns.module.scss";
 
-const HistoryHeaderBtns = ({onOpenPeriodList}) => {
+const HistoryHeaderBtns = ({ onOpenPeriodList, touchedPeriodTitle }) => {
   return (
     <div className={s.container}>
       <LabelInput
         className={s.labelBt}
         type="button"
         name="selectPeriod"
-        value={"День"}  
-        cbOnClick={onOpenPeriodList}      
+        value={touchedPeriodTitle}
+        cbOnClick={onOpenPeriodList}
       />
       <button type="button">diagram icon</button>
     </div>
--- src/components/TransactionsHistoryPage/TransactionsHistoryPage.jsx
+++ src/components/TransactionsHistoryPage/TransactionsHistoryPage.jsx
@@ -6,9 +6,8 @@ import LabelInput from "../_share/LabelInput/LabelInput";
 import Section from "../_share/Section/Section";
 import BaseSection from "../_share/BaseSection/BaseSection";
 import HistoryHeaderBtns from "../HistoryHeaderBtns/HistoryHeaderBtns";
-import MenuList from "../MenuList/MenuList"
-import periodList from "../../assets/periodList.json"
-
+import MenuList from "../MenuList/MenuList";
+import periodList from "../../assets/periodList.json";
 
 const dataCatList = [
   {
@@ -28,31 +27,43 @@ const dataCatList = [
   },
 ];
 
-
 class TransactionsHistoryPage extends Component {
- state = {
-   isOpenPeriodList: false,   
- }
-  onOpenPeriodList = () => {
-  this.setState({isOpenPeriodList: true})
-  }
-
-  render  () {
-    const {isOpenPeriodList} = this.state
-    const { handleReturnToMainPage, dataCatList } = this.props    
+  state = {
+    isOpenPeriodList: false,
+    touchedPeriod: periodList[0],
+  };
+  onTogglePeriodList = () => {
+    this.setState((prevState) => {
+      return { isOpenPeriodList: !prevState.isOpenPeriodList };
+    });
+  };
+  onChangeTouchedPeriod = (periodObj) => {
+    this.setState({ touchedPeriod: periodObj });
+    this.onTogglePeriodList();
+  };
+  render() {
+    const { isOpenPeriodList, touchedPeriod } = this.state;
+    const { handleReturnToMainPage, dataCatList } = this.props;
     const allSum = dataCatList.reduce((acc, { sum }) => acc + Number(sum), 0);
-     return (
-    <BaseSection>
-      <GoBackHeader handleGoBack={handleReturnToMainPage}>
-        <HistoryHeaderBtns onOpenPeriodList={this.onOpenPeriodList}/>
-        {isOpenPeriodList && <MenuList menuList={periodList} />}
-      </GoBackHeader>
-      <DatePaginator />
-      <HistoryTable allSum={allSum} dataCatList={dataCatList} />
-    </BaseSection>
-  );
-  } 
-
-};
+    return (
+      <BaseSection>
+        <GoBackHeader handleGoBack={handleReturnToMainPage}>
+          <HistoryHeaderBtns
+            onOpenPeriodList={this.onTogglePeriodList}
+            touchedPeriodTitle={touchedPeriod.title}
+          />
+          {isOpenPeriodList && (
+            <MenuList
+              onChangeTouchedPeriod={this.onChangeTouchedPeriod}
+              menuList={periodList}
+            />
+          )}
+        </GoBackHeader>
+        <DatePaginator />
+        <HistoryTable allSum={allSum} dataCatList={dataCatList} />
+      </BaseSection>
+    );
+  }
+}
 
 export default TransactionsHistoryPage;
